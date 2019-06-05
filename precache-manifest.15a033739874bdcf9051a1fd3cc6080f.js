ab52a3b58c67ee215b9f6faf8830c6d5fbf2d7af
Víctor Navarro <vicnala@gmail.com>
2019-06-05 12:00:19 +0200

    Updates

---
+++ precache-manifest.15a033739874bdcf9051a1fd3cc6080f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f035e32f28ad1be76f1a7b603533b7b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "663a2e2b312f02d7009b",
+    "url": "/portfolio/static/css/main.41b0ed57.chunk.css"
+  },
+  {
+    "revision": "875148d7be854731b32c",
+    "url": "/portfolio/static/js/2.5d666683.chunk.js"
+  },
+  {
+    "revision": "663a2e2b312f02d7009b",
+    "url": "/portfolio/static/js/main.f1f3b370.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "20e4fd7eed6831c1778aa056a32b3244",
+    "url": "/portfolio/static/media/reem-kufi-v4-latin-regular.20e4fd7e.woff"
+  },
+  {
+    "revision": "d8f8811d59f2a4935f145012efb7e98f",
+    "url": "/portfolio/static/media/reem-kufi-v4-latin-regular.d8f8811d.woff2"
+  },
+  {
+    "revision": "9a8a1f8c8f9860224f449c21a3bd76d4",
+    "url": "/portfolio/static/media/roboto-100.9a8a1f8c.woff2"
+  },
+  {
+    "revision": "c0cc9c92fb877993ea6d421c31d33e09",
+    "url": "/portfolio/static/media/roboto-300.c0cc9c92.woff2"
+  },
+  {
+    "revision": "ece6673e477b4d7aca12f04dace5ed60",
+    "url": "/portfolio/static/media/roboto-400.ece6673e.woff2"
+  },
+  {
+    "revision": "2e17a64836c0412759543f90460d0ed1",
+    "url": "/portfolio/static/media/roboto-900.2e17a648.woff2"
+  }
+]);
